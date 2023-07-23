89aa797c4b54fffa0033a82d5723c7ab58d9e155
Hyounbum <znzlwhdi3@gmail.com>
2023-07-23 17:31:41 +0900

    hb
    hi

--- Intermediate/Build/Win64/MainEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Intermediate/Build/Win64/MainEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.29.30151
-#include "C:/Users/user/Desktop/Unreal_Assets/Parkour/Intermediate/Build/Win64/MainEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/git/Main/Intermediate/Build/Win64/MainEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Intermediate/Build/Win64/UE4Editor/Inc/Main/MainGameModeBase.generated.h
+++ Intermediate/Build/Win64/UE4Editor/Inc/Main/MainGameModeBase.generated.h
@@ -13,10 +13,10 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define MAIN_MainGameModeBase_generated_h
 
-#define Parkour_Source_Main_MainGameModeBase_h_15_SPARSE_DATA
-#define Parkour_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS
-#define Parkour_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
-#define Parkour_Source_Main_MainGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+#define Main_Source_Main_MainGameModeBase_h_15_SPARSE_DATA
+#define Main_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS
+#define Main_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Main_Source_Main_MainGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAMainGameModeBase(); \
 	friend struct Z_Construct_UClass_AMainGameModeBase_Statics; \
@@ -25,7 +25,7 @@ public: \
 	DECLARE_SERIALIZER(AMainGameModeBase)
 
 
-#define Parkour_Source_Main_MainGameModeBase_h_15_INCLASS \
+#define Main_Source_Main_MainGameModeBase_h_15_INCLASS \
 private: \
 	static void StaticRegisterNativesAMainGameModeBase(); \
 	friend struct Z_Construct_UClass_AMainGameModeBase_Statics; \
@@ -34,7 +34,7 @@ public: \
 	DECLARE_SERIALIZER(AMainGameModeBase)
 
 
-#define Parkour_Source_Main_MainGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+#define Main_Source_Main_MainGameModeBase_h_15_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AMainGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMainGameModeBase) \
@@ -47,7 +47,7 @@ private: \
 public:
 
 
-#define Parkour_Source_Main_MainGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+#define Main_Source_Main_MainGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AMainGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
 private: \
@@ -60,28 +60,28 @@ public: \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMainGameModeBase)
 
 
-#define Parkour_Source_Main_MainGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
-#define Parkour_Source_Main_MainGameModeBase_h_12_PROLOG
-#define Parkour_Source_Main_MainGameModeBase_h_15_GENERATED_BODY_LEGACY \
+#define Main_Source_Main_MainGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Main_Source_Main_MainGameModeBase_h_12_PROLOG
+#define Main_Source_Main_MainGameModeBase_h_15_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	Parkour_Source_Main_MainGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
-	Parkour_Source_Main_MainGameModeBase_h_15_SPARSE_DATA \
-	Parkour_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS \
-	Parkour_Source_Main_MainGameModeBase_h_15_INCLASS \
-	Parkour_Source_Main_MainGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	Main_Source_Main_MainGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Main_Source_Main_MainGameModeBase_h_15_SPARSE_DATA \
+	Main_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS \
+	Main_Source_Main_MainGameModeBase_h_15_INCLASS \
+	Main_Source_Main_MainGameModeBase_h_15_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define Parkour_Source_Main_MainGameModeBase_h_15_GENERATED_BODY \
+#define Main_Source_Main_MainGameModeBase_h_15_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	Parkour_Source_Main_MainGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
-	Parkour_Source_Main_MainGameModeBase_h_15_SPARSE_DATA \
-	Parkour_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
-	Parkour_Source_Main_MainGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
-	Parkour_Source_Main_MainGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	Main_Source_Main_MainGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Main_Source_Main_MainGameModeBase_h_15_SPARSE_DATA \
+	Main_Source_Main_MainGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Main_Source_Main_MainGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Main_Source_Main_MainGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -89,7 +89,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 template<> MAIN_API UClass* StaticClass<class AMainGameModeBase>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID Parkour_Source_Main_MainGameModeBase_h
+#define CURRENT_FILE_ID Main_Source_Main_MainGameModeBase_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
